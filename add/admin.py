2389ed50bffbc4012d8d32fdda1a2b2b1b4104d3
kudr.max <mdkudryashov@edu.hse.ru>
2023-03-29 21:57:01 +0300

    Added site administration

--- add/admin.py
+++ add/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FromRandomTruthTable
+from .models import RandLevel
+from .models import NumOperations
+from .models import GeneticPlayback
+from .models import GeneticSelection
+from .models import GeneticMutation
+
+admin.site.register(FromRandomTruthTable)
+admin.site.register(RandLevel)
+admin.site.register(NumOperations)
+admin.site.register(GeneticPlayback)
+admin.site.register(GeneticSelection)
+admin.site.register(GeneticMutation)
