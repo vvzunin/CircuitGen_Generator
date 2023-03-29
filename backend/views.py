d6463b6863097ddc56deb4ec1ced0561e2e08756
kudr.max <mdkudryashov@edu.hse.ru>
2023-03-29 20:36:26 +0300

    Linked react and django. Now we can open frontend using django

--- backend/views.py
+++ backend/views.py
@@ -1,30 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from .serializer import *
 
-class TruthTableViewSet(viewsets.ModelViewSet):
-    queryset = TruthTable.objects.all()
-    serializer_class = TruthTableSerializer
-
-class RandLevelViewSet(viewsets.ModelViewSet):
-    queryset = RandLevel.objects.all()
-    serializer_class = RandLevelSerializer
-
-class NumOperationsViewSet(viewsets.ModelViewSet):
-    queryset = NumOperations.objects.all()
-    serializer_class = NumOperationsSerializer
-
-class GeneticPlaybackViewSet(viewsets.ModelViewSet):
-    queryset = GeneticPlayback.objects.all()
-    serializer_class = GeneticPlaybackSerializer
-
-class GeneticMutationViewSet(viewsets.ModelViewSet):
-    queryset = GeneticMutation.objects.all()
-    serializer_class = GeneticMutationSerializer
-
-class GeneticSelectionViewSet(viewsets.ModelViewSet):
-    queryset = GeneticSelection.objects.all()
-    serializer_class = GeneticSelectionSerializer
-
-def main(request):
-    return render(request, 'backend/index.html')
+def front(request):
+    context = { }
+    return render(request, "index.html", context)
--- django_project/asgi.py
+++ django_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for project_1290 project.
+ASGI config for django_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_1290.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 application = get_asgi_application()
