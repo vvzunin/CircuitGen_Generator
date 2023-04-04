530b168130bb0c48d4a01b856b5c15f4bc49ecfe
kudr.max <mdkudryashov@edu.hse.ru>
2023-04-04 19:09:51 +0300

    Changed models of Add app

--- add/serializers.py
+++ add/serializers.py
@@ -6,3 +6,4 @@ class AddParameterSerializer(serializers.ModelSerializer):
     class Meta:
         model = AddParameter
         fields = "__all__"
+        # extra_kwargs = {field.name:{'required': False, 'allow_null': True} for field in AddParameter._meta.get_fields()}
