ea3ebe7bc564b807e0dee4ee442ca86fcb5345ed
Radzhabov Rustam <rrradzhabov@edu.hse.ru>
2023-04-07 19:55:06 +0400

    Updated tests for TruthTable.cpp

--- Generator/source/tests/TruthTableTests.cpp
+++ Generator/source/tests/TruthTableTests.cpp
@@ -16,3 +16,9 @@ TEST(EqualOperationOfTruthTable, DifferentFromEachOther)
     EXPECT_EQ((TruthTable{ 3,  1 , std::vector<std::vector<bool>>({{true},{true} ,{true} , {false} , {true},{true} ,{true} , {false}}) } == TruthTable{ 3, 1 , std::vector<std::vector<bool>>({{true},{true} , {true} , {false} , {true},{true} ,{true} , {true}}) }), false);
     EXPECT_EQ((TruthTable{ 1,  1 , std::vector<std::vector<bool>>({{false},{false}}) } == TruthTable{ 1, 1 , std::vector<std::vector<bool>>({{true},{true}}) }), false);
 }
+TEST(ConvToBinaryOfTruthTable, NormalTest)
+{
+    EXPECT_EQ((TruthTable{ 1  , 1 , std::vector<std::vector<bool>>({{true} , {true} }) }).convToBinary(), std::vector<std::vector<bool>>({ {false} , {true} }));
+    EXPECT_EQ((TruthTable{ 2  , 1 , std::vector<std::vector<bool>>({{true} , {false}, {false} , {false} }) }).convToBinary(), std::vector<std::vector<bool>>({ { false , false }, { false , true }, { true, false }, {true , true} }));
+    EXPECT_EQ((TruthTable{ 3  , 1 , std::vector<std::vector<bool>>({}) }).convToBinary(), std::vector<std::vector<bool>>({ { false , false , false },  { false , false , true },{ false , true , false }, {false , true , true} , {true , false , false} , {true , false , true} , {true, true ,false} , {true , true ,true} }));
+}
