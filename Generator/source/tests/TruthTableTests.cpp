ab21c6746d36cf259fc0408ec2f4686db230c597
Radzhabov Rustam <rrradzhabov@edu.hse.ru>
2023-04-07 19:54:40 +0400

    Updated tests for TruthTable.cpp

--- Generator/source/tests/TruthTableTests.cpp
+++ Generator/source/tests/TruthTableTests.cpp
@@ -10,3 +10,9 @@ TEST(EqualOperationOfTruthTable, EqualToEachOther)
    EXPECT_EQ((TruthTable{ 2,  2 , std::vector<std::vector<bool>>({{true , true},{true , true} ,{true , true} , {false , false}}) } == TruthTable{ 2,  2 , std::vector<std::vector<bool>>({{true , true},{true , true} ,{true , true} , {false , false}}) }), true);
 
 }
+TEST(EqualOperationOfTruthTable, DifferentFromEachOther)
+{
+    EXPECT_EQ((TruthTable{ 1,  1 , std::vector<std::vector<bool>>({{true},{true}}) } == TruthTable{ 1, 1 , std::vector<std::vector<bool>>({{true},{false}}) }), false);
+    EXPECT_EQ((TruthTable{ 3,  1 , std::vector<std::vector<bool>>({{true},{true} ,{true} , {false} , {true},{true} ,{true} , {false}}) } == TruthTable{ 3, 1 , std::vector<std::vector<bool>>({{true},{true} , {true} , {false} , {true},{true} ,{true} , {true}}) }), false);
+    EXPECT_EQ((TruthTable{ 1,  1 , std::vector<std::vector<bool>>({{false},{false}}) } == TruthTable{ 1, 1 , std::vector<std::vector<bool>>({{true},{true}}) }), false);
+}
