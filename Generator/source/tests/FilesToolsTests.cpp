3bf05f3a81158926451e2d08863937d6957d15db
Rustam Radzhabov <rrradzhabov@edu.hse.ru>
2023-04-12 20:43:30 +0300

    Added test DirectoriesAndFilesExist to FileToolsTests.cpp

--- Generator/source/tests/FilesToolsTests.cpp
+++ Generator/source/tests/FilesToolsTests.cpp
@@ -82,3 +82,47 @@ TEST(FileTools, NoDirectoriesOnlyFiles)
     }
     std::filesystem::remove_all(tmpPath.string());//Clean our directories.
 }
+TEST(FileTools, DirectoriesAndFilesExist)
+{
+
+    fs::path tmpPath = (fs::temp_directory_path() / "test");//Created temporary guaranted directory to deal with
+
+    std::string tmp; // This variable exist because otherwise we will not be able push tmpPath.string() to method getDirectories(Here it is gonna be)
+
+    std::fstream file; // Created fstream variable to have the possibility to create files into temp directory to test the fucntion on counting only directories.
+
+    fs::create_directories(tmpPath / "6");// Just created directories within temporary directory . The order does not matter. You will see below why
+    fs::create_directories(tmpPath / "5");
+    fs::create_directories(tmpPath / "4");
+    fs::create_directories(tmpPath / "3");
+    fs::create_directories(tmpPath / "2");
+    fs::create_directories(tmpPath / "1");
+
+
+    std::vector<std::string> correctVec =
+    { (tmpPath / "1").string() , (tmpPath / "2").string()
+        , (tmpPath / "3").string() , (tmpPath / "4").string()
+        , (tmpPath / "5").string() , (tmpPath / "6").string() }; // Created vector that's gonna be compared with the result vector we wanna test
+
+    // Created ten empty txt files in the current directory to make sure that the function does not count files.
+    for (int i = 1; i <= 10; i++)
+    {
+        std::ofstream file;
+        std::string str = std::to_string(i) + ".txt";//To prevent warning from below line
+        const char* nameTxtFile(str.c_str());
+        file.open((tmpPath / nameTxtFile).string());// Just create a file with name i.txt
+        file.close();
+    }
+
+    tmp = tmpPath.string();// get the path to the current directory which contain created directories
+
+    std::vector<std::string> resVec = getDirectories(tmp);// Vector which we wanna test
+
+    std::sort(correctVec.begin(), correctVec.end());// If the elements of correctVec and resVec are the same
+    std::sort(resVec.begin(), resVec.end());        // after sort they are both will be equal to each other.
+
+    EXPECT_EQ(resVec, correctVec);
+
+    std::filesystem::remove_all(tmpPath.string());//Delete the current directory we work with.
+
+}
