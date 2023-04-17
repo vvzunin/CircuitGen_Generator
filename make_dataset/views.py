cfecdfd5b20732503741611b2ce7569fb58b3312
kudr.max <mdkudryashov@edu.hse.ru>
2023-04-17 13:05:06 +0300

    Added dataset_id for make_dataset function

--- make_dataset/views.py
+++ make_dataset/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -5,11 +7,14 @@ from add_parameter.views import *
 
 
 def make_dataset(request):
-    cpp_function(AddParameter.objects.all().values())
+    data = AddParameter.objects.all().values()
+    dataset_id = "%032x" % random.getrandbits(128)
+    cpp_function(data, dataset_id)
     return HttpResponse("Ok")
 
 
-def cpp_function(data):
+def cpp_function(data, dataset_id):
     # run cpp code here
-    print(data)
+    # print(dataset_id)
+    # print(data)
     return HttpResponse("https://youtu.be/dQw4w9WgXcQ")
