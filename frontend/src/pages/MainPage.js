080faee670cbf3a7812ba5ee7a534b9a303c8d18
Stepan <ssavdeyuk@edu.hse.ru>
2023-04-18 23:41:25 +0300

    mock api test backend

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,18 +1,52 @@
 import React from 'react'
+import axios from 'axios';
 import { Link } from 'react-router-dom'
 
 import Parametr from "../components/Parametr";
 import Scheme from "../components/Scheme";
 
 const MainPage = () => {
-  return (
-    <div className="content__wrapper">
+
+	const [generatorParametrs, setGeneratorParametrs] = React.useState(null);
+
+	console.log(generatorParametrs)
+
+	const getGeneratorParametrs = () => {
+		axios.get('https://641051b7e1212d9cc930179a.mockapi.io/generatorParametrs')
+		.then(({data}) => setGeneratorParametrs(data))
+		.catch(e => console.log(e));
+	}
+
+	const deleteParametr = (id) => {
+		axios.delete(`https://641051b7e1212d9cc930179a.mockapi.io/generatorParametrs/${id}`)
+		.then(() => {
+			getGeneratorParametrs();
+		})
+		.catch(e => console.log(e));
+	}
+
+	React.useEffect(() => {
+		getGeneratorParametrs();
+	}, []);
+
+	return (
+	<div className="content__wrapper">
 			<div className="content__left">
 				<h3>Параметры генерации</h3>
 				<div className="content pb75">
 					<div className="content__scroll">
-						<Parametr/>
-						<Parametr/>
+						{generatorParametrs && generatorParametrs.map((item, i) => {
+							return <Parametr 
+								id={item.id} 
+								method={item.method}
+								minInCount={item.minInCount}
+								maxInCount={item.maxInCount}
+								minOutCount={item.minOutCount}
+								maxOutCount={item.maxOutCount}
+								repeats={item.repeats}
+								deleteParametr={() => deleteParametr(item.id)}
+							/>
+						})}
 					</div>
 					<div className="content__buttons">
 						<Link to='/add' className="content__add-parametr">Добавить параметр</Link>
