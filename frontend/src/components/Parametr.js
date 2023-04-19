d98d3f8b7a39b9cac3f34a7a5c985023a47777c5
Stepan <ssavdeyuk@edu.hse.ru>
2023-04-19 13:43:36 +0300

    add parametr status

--- frontend/src/components/Parametr.js
+++ frontend/src/components/Parametr.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const Parametr = ({id, method, minInCount, maxInCount, minOutCount, maxOutCount, repeats, deleteParametr}) => {
+import gearImg from '../assets/gear.svg';
+import select from '../assets/select.svg';
+import noselect from '../assets/noselect.svg';
+
+const Parametr = ({id, method, minInCount, maxInCount, minOutCount, maxOutCount, repeats, deleteParametr, gear}) => {
 
     const [isOpen, setIsOpen] = React.useState(false);
+
+    const [status, setStatus] = React.useState('noselect');
+
+    React.useEffect(() => {
+        if (gear) {
+            setStatus('gear');
+        }
+    }, [gear])
+    
 
     return (
     <div className='parametr__wrapper'>
@@ -11,6 +24,15 @@ const Parametr = ({id, method, minInCount, maxInCount, minOutCount, maxOutCount,
             Параметр генерации #{id}
         </div>
         <div className="parametr__buttons">
+            {status && (status == 'gear') && <div className="parametr__status gear">
+                <img src={gearImg}/>
+            </div>}
+            {status && (status == 'select') && <div className="parametr__status select" onClick={() => setStatus('noselect')}>
+                <img src={select}/>
+            </div>}
+            {status && (status == 'noselect') && <div className="parametr__status noselect" onClick={() => setStatus('select')}>
+                <img src={noselect}/>
+            </div>}
             <button className='parametr__more' onClick={() => setIsOpen(!isOpen)}>{isOpen ? 'Скрыть' : 'Подробнее'}</button>
             <button className='parametr__delete'onClick={() => deleteParametr()}>Удалить</button>
         </div>
