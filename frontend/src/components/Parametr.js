9e5cc4b2f36322f99a8c4a888aa0a7c19bd61b7b
Stepan <ssavdeyuk@edu.hse.ru>
2023-05-01 19:36:22 +0300

    set different about info for each generator parametr

--- frontend/src/components/Parametr.js
+++ frontend/src/components/Parametr.js
@@ -46,36 +46,56 @@ const Parametr = ({dataItem, deleteParametr, gear}) => {
                 <li>Минимальное количество выходов: {dataItem.min_out}</li>
                 <li>Максимальное количество выходов: {dataItem.max_out}</li>
                 <li>Количество повторений каждой комбинации: {dataItem.repeat_n}</li>
-                <li>Максимальное количество уровней: {dataItem.max_level}</li>
-                <li>Максимальное количество элементов: {dataItem.max_elem}</li>
-                <li>Ограничение генерации: {dataItem.limit ? "true" : "false"}</li>
-                <li>CNFF: {dataItem.CNFF ? "true" : "false"}</li>
-                <li>CNFT: {dataItem.CNFT ? "true" : "false"}</li>
-                <li>Оставлять пустые выходы: {dataItem.leave_empty_out ? "true" : "false"}</li>
-                <li>And: {dataItem.num_and}</li>
-                <li>Nand: {dataItem.num_nand}</li>
-                <li>Or: {dataItem.num_or}</li>
-                <li>Not: {dataItem.num_not}</li>
-                <li>Nor: {dataItem.num_nor}</li>
-                <li>Buf: {dataItem.num_buf}</li>
-                <li>Xor: {dataItem.num_xor}</li>
-                <li>Xnor: {dataItem.num_xnor}</li>
-                <li>Размер популяции: {dataItem.population}</li>
-                <li>Количество циклов: {dataItem.cycles}</li>
-                <li>Коэффициент выхода: {dataItem.u_out}</li>
-                <li>Тип хромосомы: {dataItem.chromosome_type}</li>
-                <li>Тип отбора родителей: {dataItem.selection_type_parent}</li>
-                <li>Размер турнира: {dataItem.tour_size}</li>
-                <li>Тип воиспроизведения: {dataItem.playback_type}</li>
-                <li>Reference points: {dataItem.ref_points}</li>
-                <li>MaskProbability: {dataItem.mask_prob}</li>
-                <li>RecombinationNumber: {dataItem.rec_num}</li>
-                <li>Тип мутации: {dataItem.mut_type}</li>
-                <li>Вероятность мутации: {dataItem.mut_chance}</li>
-                <li>Тип обмена: {dataItem.swap_type}</li>
-                <li>Соотношение в таблице истинности: {dataItem.ratio_in_table}</li>
-                <li>Тип отбора: {dataItem.selection_type}</li>
-                <li>Количество выживших: {dataItem.surv_num}</li>
+                {dataItem.type_of_generation === "From Random Truth Table" && <>
+                    <li>Ограничение генерации: {dataItem.limit ? "true" : "false"}</li>
+                    <li>CNFF: {dataItem.CNFF ? "true" : "false"}</li>
+                    <li>CNFT: {dataItem.CNFT ? "true" : "false"}</li>
+                </>}
+                {dataItem.type_of_generation === "Rand Level" && <>
+                    <li>Максимальное количество уровней: {dataItem.max_level}</li>
+                    <li>Максимальное количество элементов: {dataItem.max_elem}</li>
+                </>}
+                {dataItem.type_of_generation === "Num Operation" && <>
+                    <li>Оставлять пустые выходы: {dataItem.leave_empty_out ? "true" : "false"}</li>
+                    <li>And: {dataItem.num_and}</li>
+                    <li>Nand: {dataItem.num_nand}</li>
+                    <li>Or: {dataItem.num_or}</li>
+                    <li>Not: {dataItem.num_not}</li>
+                    <li>Nor: {dataItem.num_nor}</li>
+                    <li>Buf: {dataItem.num_buf}</li>
+                    <li>Xor: {dataItem.num_xor}</li>
+                    <li>Xnor: {dataItem.num_xnor}</li>
+                </>}
+                {dataItem.type_of_generation === "Genetic reproduction" && <>
+                    <li>Размер популяции: {dataItem.population}</li>
+                    <li>Количество циклов: {dataItem.cycles}</li>
+                    <li>Коэффициент выхода: {dataItem.u_out}</li>
+                    <li>Тип хромосомы: {dataItem.chromosome_type}</li>
+                    <li>Тип отбора родителей: {dataItem.selection_type_parent}</li>
+                    <li>Размер турнира: {dataItem.tour_size}</li>
+                    <li>Тип воиспроизведения: {dataItem.playback_type}</li>
+                    <li>Reference points: {dataItem.ref_points}</li>
+                    <li>MaskProbability: {dataItem.mask_prob}</li>
+                    <li>RecombinationNumber: {dataItem.rec_num}</li>
+                </>}
+                {dataItem.type_of_generation === "Genetic mutation" && <>
+                    <li>Размер популяции: {dataItem.population}</li>
+                    <li>Количество циклов: {dataItem.cycles}</li>
+                    <li>Коэффициент выхода: {dataItem.u_out}</li>
+                    <li>Тип хромосомы: {dataItem.chromosome_type}</li>
+                    <li>Тип мутации: {dataItem.mut_type}</li>
+                    <li>Вероятность мутации: {dataItem.mut_chance}</li>
+                    <li>Тип обмена: {dataItem.swap_type}</li>
+                    <li>Соотношение в таблице истинности: {dataItem.ratio_in_table}</li>
+                </>}
+                {dataItem.type_of_generation === "Genetic selection" && <>
+                    <li>Размер популяции: {dataItem.population}</li>
+                    <li>Количество циклов: {dataItem.cycles}</li>
+                    <li>Коэффициент выхода: {dataItem.u_out}</li>
+                    <li>Тип хромосомы: {dataItem.chromosome_type}</li>
+                    <li>Тип отбора: {dataItem.selection_type}</li>
+                    <li>Количество выживших: {dataItem.surv_num}</li>
+                </>}
             </ul>
         </div>
     </div>
