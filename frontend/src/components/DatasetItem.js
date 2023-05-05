7666e03c6cfa3076bbc41cd56fc898963542a642
Stepan <ssavdeyuk@edu.hse.ru>
2023-05-05 20:02:22 +0300

    generated datasets with mock api

--- frontend/src/components/DatasetItem.js
+++ frontend/src/components/DatasetItem.js
@@ -1,16 +1,41 @@
 import React from 'react'
 import Scheme from './Scheme'
 
-const DatasetItem = ({id}) => {
+const DatasetItem = ({id, parameters, currentProgress}) => {
+
+  const [status, setStatus] = React.useState(null);
+
+  React.useEffect(() => {
+    if (!currentProgress) {
+      setStatus('error');
+    } else {
+      if (currentProgress.ready == currentProgress.total) {
+        setStatus('equal')
+      } else {
+        setStatus('generation')
+      }
+    }
+  }, [])
+  
+
   return (
     <div className='dataset__wrapper'>
         <div className="dataset__info">
             <span>Датасет #{id}</span>
+            {
+              (status === "error") && <p>Статус генерации: ошибка генерации</p>
+            }
+            {
+              (status === "equal") && <p>Статус генерации: генерация завершена</p>
+            }
+            {
+              (status === "generation") && <p>Статус генерации: сгенерировано {currentProgress?.ready} из {currentProgress?.total} схем</p>
+            }
         </div>
         <div className="dataset__content">
-            <Scheme/>
-            <Scheme/>
-            <Scheme/>
+            {parameters && parameters.map((item, i) => {
+              return <Scheme id={item} key={i}/>
+            })}
         </div>
     </div>
   )
