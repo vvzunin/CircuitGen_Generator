a23a54212b8770d6cc52f89c50328ed1e6e14635
kudr.max <mdkudryashov@edu.hse.ru>
2023-05-07 14:24:48 +0300

    Added SynologyDrive and changed gitignore

---
+++ data/SynologyDrive/test.py
@@ -0,0 +1,14 @@
+from synology_drive_api.drive import SynologyDrive
+
+
+NAS_USER = 'project1290'
+NAS_PASS = '~.*{*$7]NJ1[pS`\\'
+NAS_IP = 'vvzunin.me'
+NAS_PORT = 10003
+dsm_version = '7'
+
+with SynologyDrive(NAS_USER, NAS_PASS, NAS_IP, NAS_PORT, dsm_version=dsm_version) as synd:
+    a = synd.list_folder('/team-folders/circuits') # Папка circuits
+    items = a['data']['items']
+    for i in items:
+        print(i['name'])
