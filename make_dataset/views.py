f0acc6cc576067946efcf4e24163e1544d750c9c
kudr.max <mdkudryashov@edu.hse.ru>
2023-05-09 00:31:44 +0300

    Made upload_to_synology upload json and png too

--- make_dataset/views.py
+++ make_dataset/views.py
@@ -6,7 +6,6 @@ from django.http import JsonResponse
 from django.http import HttpResponse
 from add_parameter.views import *
 import json
-import os
 
 from rest_framework import viewsets
 
@@ -16,11 +15,10 @@ from .serializers import DatasetSerializer
 from pathlib import Path
 import glob
 
-from .upload_to_synology import upload_to_synology
-# from data.SynologyDrive.synology_drive_api.drive import SynologyDrive
-# from synology_drive_api.drive import SynologyDrive
+import io
+import os
 
-import ssl
+from synology_drive_api.drive import SynologyDrive
 
 
 class DatasetList(viewsets.ModelViewSet):
@@ -52,10 +50,10 @@ def add_dataset(request):
     # cpp_function(parameters_of_generation, dataset_id)
 
     # запус Yosys
-    # make_image_from_verilog(dataset_id)
+    make_image_from_verilog(9999)
 
     # загрузка на яндекс диск
-    upload_to_synology()
+    # upload_to_synology(dataset_id)
 
     # изменение ссылки на synology на актуальную
     # ДОПИСАТЬ
@@ -73,7 +71,8 @@ def cpp_function(parameters_of_generation, dataset_id):
         obj['dataset_id'] = dataset_id
     with open(f'temp_for_json/data_{dataset_id}.json', 'w', encoding='utf-8') as f:
         json.dump(parameters_of_generation, f, ensure_ascii=False, indent=4)
-    subprocess.Popen(f"./Generator/source/build/prog --json_path=./temp_for_json/data_{dataset_id}.json", shell=True).wait()
+    subprocess.Popen(f"./Generator/source/build/prog --json_path=./temp_for_json/data_{dataset_id}.json",
+                     shell=True).wait()
 
 
 def make_image_from_verilog(dataset_id):
@@ -118,7 +117,30 @@ def in_total_function(obj):
     for param in list_of_param:
         id_of_parameter = param["id_of_parameter"]
         data_param = AddParameter.objects.values().get(id=id_of_parameter)
-        in_total += (data_param["max_in"] - data_param["min_in"] + 1) * (data_param["max_out"] - data_param["min_out"] + 1) * data_param["repeat_n"]
+        in_total += (data_param["max_in"] - data_param["min_in"] + 1) * (
+                    data_param["max_out"] - data_param["min_out"] + 1) * data_param["repeat_n"]
         if data_param["CNFF"] is True or data_param["CNFT"] is True:
             in_total *= 2
     return in_total
+
+
+def upload_to_synology(dataset_id):
+    NAS_USER = 'project1290'
+    NAS_PASS = '~.*{*$7]NJ1[pS`\\'
+    NAS_IP = 'vvzunin.me'
+    NAS_PORT = 10003
+    dsm_version = '7'
+
+    with SynologyDrive(NAS_USER, NAS_PASS, NAS_IP, NAS_PORT, dsm_version=dsm_version) as synd:
+        dataset_id = str(dataset_id)
+        dataset_dir = './dataset/' + dataset_id
+        for param in os.listdir(dataset_dir):
+            param_dir = '/'.join([dataset_dir, param])
+            for circuit in os.listdir(param_dir):
+                extension_lst = ['.v', '.json', '.png']
+                for extension in extension_lst:
+                    verilog_path = '/'.join([param_dir, circuit, circuit]) + extension
+                    with open(verilog_path, 'rb') as file:
+                        bfile = io.BytesIO(file.read())
+                        bfile.name = '/'.join([dataset_id, param, circuit, circuit]) + extension
+                        synd.upload_file(bfile, dest_folder_path='/team-folders/circuits/')
