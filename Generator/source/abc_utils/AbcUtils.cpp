8cb3be4eefd5e397eb89e3c1972d2bde3acd3afb
Fuuulkrum7 <ilka747428@gmail.com>
2023-12-29 03:25:55 +0500

    Create abc_utils

---
+++ Generator/source/abc_utils/AbcUtils.cpp
@@ -0,0 +1,114 @@
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <cassert>
+
+#include "AbcUtil.h"
+
+// declare of value of static var
+inline const std::string AbcUtil::abc_word = "abc ";
+
+// TODO at this moment can execute only three commands, in fact one operation
+// make it to read more, if it would ne neccessary
+inline bool AbcUtil::standartExecutor(
+            std::string command,
+            int i_len, 
+            int o_len, 
+            int first_command, 
+            int second_command, 
+            int third_command) 
+{
+    FILE *abc_output;
+    char out[40];      
+
+    // executing command with own read stream
+    abc_output = popen(command.c_str(), "r");
+
+    std::string result;
+    // parsing output
+    while (fgets(out, sizeof(out), abc_output)) {
+        result += out;
+    }
+
+    // looking for each command execution
+    int read_pos = result.find(abc_word, 0);
+    int strash_pos = result.find(abc_word, read_pos + 1);
+    int write_pos = result.find(abc_word, strash_pos + 1);
+    int end_pos = result.find(abc_word, write_pos + 1);
+
+    // если хоть где-то ошибка была
+    if (
+        read_pos == std::string::npos || 
+        strash_pos == std::string::npos || 
+        write_pos == std::string::npos || 
+        end_pos == std::string::npos
+    ) 
+    {
+        std::cout << "Something went wrong during files parsing in AbcUtil\n";
+        return false;
+    }
+
+    // 2 - one space and one \n
+    int delta = abc_len + first_command + 2 + i_len;
+
+    // If something went wrong during read
+    // and abc wrote something
+    if (strash_pos - read_pos > delta) {
+        std::cerr << "Incorrect read: " << result.substr(read_pos + delta, strash_pos - read_pos - delta) << '\n';
+        return false;
+    }
+
+    delta = abc_len + second_command + 1;
+    // If there were some problems with strash
+    if (write_pos - strash_pos > delta) {
+        std::cerr << "Incorrect strash/balance: " << result.substr(strash_pos + delta, write_pos - strash_pos - delta) << '\n';
+        return false;
+    }
+
+    delta = abc_len + third_command + 2 + o_len;
+    // If there were problems during writing
+    if (end_pos - write_pos > delta) {
+        std::cerr << "Incorrect write: " << result.substr(write_pos + delta, end_pos - write_pos - delta) << '\n';
+        return false;
+    }
+
+    return true;
+}
+
+inline bool AbcUtil::verilogToAiger(std::string i_inpuFileName, std::string i_outpuFileName) {
+    std::string command = "(echo \"read_verilog " + i_inpuFileName + "\"";
+    command += "&& echo \"strash\" && echo \"";
+    command += "write_aiger " + i_outpuFileName + "\") | abc";
+
+    return standartExecutor(
+        command, 
+        i_inpuFileName.size(), i_outpuFileName.size(), 
+        r_verilog_len, strash_len, w_aiger_len
+    );
+}
+
+inline bool AbcUtil::verilogToAiger(std::string i_inpuFileName, std::string i_outpuFileName, std::string i_directory) {
+    if (i_directory[i_directory.size() - 1] != '/')
+        i_directory += "/";
+
+    return verilogToAiger(i_directory + i_inpuFileName, i_directory + i_outpuFileName);
+}
+
+inline bool AbcUtil::aigerToVerilog(std::string i_inpuFileName, std::string i_outpuFileName) {
+    std::string command = "(echo \"read_aiger " + i_inpuFileName + "\"";
+    command += "&& echo \"strash\" && echo \"";
+    command += "write_verilog " + i_outpuFileName + "\") | abc";
+    
+    return standartExecutor(
+        command, 
+        i_inpuFileName.size(), i_outpuFileName.size(), 
+        r_aiger_len, strash_len, w_verilog_len
+    );
+}
+
+inline bool AbcUtil::aigerToVerilog(std::string i_inpuFileName, std::string i_outpuFileName, std::string i_directory) {
+    if (i_directory[i_directory.size() - 1] != '/')
+        i_directory += "/";
+
+    return aigerToVerilog(i_directory + i_inpuFileName, i_directory + i_outpuFileName);
+}
---
+++ Generator/source/abc_utils/AbcUtils.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <string>
+
+/*
+This class is made for basic work with abc.
+Using it, it is possible to read verlog file,
+read aiger file, make strash and to write file into
+verilog or aiger
+*/
+
+class AbcUtil {
+    public:
+        static bool verilogToAiger(std::string i_inpuFileName, std::string i_outpuFileName);
+        static bool verilogToAiger(
+            std::string i_inpuFileName, std::string i_outpuFileName, std::string i_directory
+        );
+
+        static bool aigerToVerilog(std::string i_inpuFileName, std::string i_outpuFileName);
+        static bool aigerToVerilog(
+            std::string i_inpuFileName, std::string i_outpuFileName, std::string i_directory
+        );
+
+        static bool balanceVerilog(std::string i_inpuFileName);
+        static bool balanceVerilog(std::string i_inpuFileName, std::string i_directory);
+
+        static bool balanceAiger(std::string i_inpuFileName);
+        static bool balanceAiger(std::string i_inpuFileName, std::string i_directory);
+
+        static bool strashVerilog(std::string i_inpuFileName);
+        static bool strashVerilog(std::string i_inpuFileName, std::string i_directory);
+
+        static bool strashAiger(std::string i_inpuFileName);
+        static bool strashAiger(std::string i_inpuFileName, std::string i_directory);
+    private:
+        // len of "abc 0*> "
+        static const int abc_len = 8;
+        // "abc *" itself
+        static const std::string abc_word;
+
+        // "read_verilog" len
+        static const int r_verilog_len = 12;
+        // "write_verilog" len
+        static const int w_verilog_len = 13;
+
+        // "read_aiger" len
+        static const int r_aiger_len = 10;
+        // "write_aiger" len
+        static const int w_aiger_len = 11;
+
+        // len of "strash"
+        static const int strash_len = 6;
+        // len of "balance"
+        static const int balance_len = 7;
+
+        static bool standartExecutor(
+            std::string command,
+            int i_len, 
+            int o_len, 
+            int first_command, 
+            int second_command, 
+            int third_command
+        );
+};
