7d818d5a05ce8ee4f8f7f9faff599725cc3e99df
Asdfds1 <ziazetdinov@list.ru>
2024-01-26 18:09:23 +0500

    Add fix in ParityTests

--- Generator/source/tests/ParityTests.cpp
+++ Generator/source/tests/ParityTests.cpp
@@ -10,8 +10,8 @@ TEST(EqualGenerateParity, EqualToEachOver)
     OrientedGraph graph_1 = S_gen_1.generatorParity(2);
     OrientedGraph graph_2 = S_gen_2.generatorParity(2);
     EXPECT_TRUE(graph_1 == graph_2);
-    OrientedGraph graph_1 = S_gen_1.generatorParity(10);
-    OrientedGraph graph_2 = S_gen_2.generatorParity(10);
+    graph_1 = S_gen_1.generatorParity(10);
+    graph_2 = S_gen_2.generatorParity(10);
     EXPECT_TRUE(graph_1 == graph_2);
 }
 TEST(EqualGenerateParity, DifferentFromEachOthe)
@@ -21,7 +21,7 @@ TEST(EqualGenerateParity, DifferentFromEachOthe)
     OrientedGraph graph_1 = S_gen_1.generatorParity(4);
     OrientedGraph graph_2 = S_gen_2.generatorParity(5);
     EXPECT_FALSE(graph_1 == graph_2);
-    OrientedGraph graph_1 = S_gen_1.generatorParity(10);
-    OrientedGraph graph_2 = S_gen_2.generatorParity(2);
-    EXPECT_FALSE(graph_1 == graph_2);
+    graph_1 = S_gen_1.generatorParity(10);
+    graph_2 = S_gen_2.generatorParity(2);
+    EXPECT_FALSE(graph_1 == graph_2);  
 }
