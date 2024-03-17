c29fb54bf29450c4f7e6ccdb3d309c5997a0d2bf
magomed <mrnavruzov@edu.hse.ru>
2024-03-17 16:54:04 +0300

    DataBaseGeneratorTests demo

---
+++ Generator/source/tests/DataBaseGeneratorTests.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "../DataBase/DataBaseGenerator.h"
+#include "../DataBase/DataBaseGeneratorParameters.h"
+
+TEST(GenerateTypeTest, EqualWithTheSameSeeds) {
+  //GenerationParameters gParams("test", "123", 10, 10, 2);
+  //DataBaseGeneratorParameters dbParams(1, 3, 1, 3, 3, FromRandomTruthTable, gParams);
+  //DataBaseGenerator generator(dbParams);
+  //generator.generateType(dbParams);
+}
