950f0833f59d1a2d7f9dc463508fdbe35e36efae
magomed <mrnavruzov@edu.hse.ru>
2024-03-24 00:40:40 +0300

    Commit before pull

--- Generator/source/tests/DataBaseGeneratorTests.cpp
+++ Generator/source/tests/DataBaseGeneratorTests.cpp
@@ -1,10 +1,27 @@
 #include <gtest/gtest.h>
+#include <filesystem>
+
 #include "../DataBase/DataBaseGenerator.h"
 #include "../DataBase/DataBaseGeneratorParameters.h"
 
-TEST(GenerateTypeTest, EqualWithTheSameSeeds) {
-  //GenerationParameters gParams("test", "123", 10, 10, 2);
-  //DataBaseGeneratorParameters dbParams(1, 3, 1, 3, 3, FromRandomTruthTable, gParams);
-  //DataBaseGenerator generator(dbParams);
-  //generator.generateType(dbParams);
-}
+/*TEST(GenerateDataBaseFromRandomTruthTable, EqualWithTheSameSeeds) {
+  GenerationParameters gParams("FromRandomTruthTable", "1", 4, 4, 8,"Generator/libs/sky130.lib", true, true, true, true);
+  DataBaseGeneratorParameters dbParams(1, 4, 1, 4, 8, FromRandomTruthTable, gParams);
+  DataBaseGenerator generator(dbParams);
+  generator.generateType(dbParams);
+  std::filesystem::remove_all("dataset/RandLevel");
+}*/
+
+/*TEST(GenerateDataBaseRandLevel, EqualWithTheSameSeeds) {
+  GenerationParameters gParams1("RandLevel", "1", 5, 5, 8);
+  DataBaseGeneratorParameters dbParams1(2, 6, 2, 6, 1, RandLevel, gParams1);
+  DataBaseGenerator generator1(dbParams1);
+  generator1.generateType(dbParams1);
+}*/
+
+/*TEST(generateDataBaseNumOperations, EqualWithTheSameSeeds) {
+  GenerationParameters gParams1("NumOperations", "1", 6, 6, 8);
+  DataBaseGeneratorParameters dbParams1(2, 6, 2, 6, 1, NumOperation, gParams1);
+  DataBaseGenerator generator1(dbParams1);
+  generator1.generateType(dbParams1);
+}*/
