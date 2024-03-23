760b0343fb2d25fec55ce4645238d8b6882116c0
Nikita Indyuchenko <nikitosik.200210@gmail.com>
2024-03-23 16:46:52 +0300

    description of the RecombinationType function

--- Generator/source/generators/Genetic/Recombination/Recombinations.h
+++ Generator/source/generators/Genetic/Recombination/Recombinations.h
@@ -6,6 +6,19 @@
 
 #include "../ChronosomeType.h"
 
+/// @brief RecombinationType TO DO: Description!!!
+/// @param i_recombinationParameters An object of the Recombination
+/// Parameters class that contains information about the type of recombination***
+/// 
+/// @param i_population vector std::vector<ChronosomeType<Type, Parameters Type>>,
+/// containing a population of chromosomes. Each element of the vector represents
+/// a chromosome of a certain type Type with the corresponding ParametersType
+/// parameters.
+/// @return vector std::vector<ChronosomeType<Type, Parameters Type>>, which
+/// represents a new population of chromosomes after applying the selected
+/// recombination method
+/// */
+
 template<typename Type, typename ParametersType>
 std::vector<ChronosomeType<Type, ParametersType>> RecombinationType
 (
