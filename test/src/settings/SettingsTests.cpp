085f35373d88686650d44b90bfa3051640e2ff43
magomed <mrnavruzov@edu.hse.ru>
2024-04-09 22:19:18 +0300

    Add new tests to SettingsTests.cpp

--- test/src/settings/SettingsTests.cpp
+++ test/src/settings/SettingsTests.cpp
@@ -16,7 +16,51 @@ std::string fileName =
 // only way to test LoadSettings when file does not exists without breaking
 // encapsulation.
 
-TEST(test_settings, test_default_load_settings) {
+TEST(SettingsTest, TestingGetInstance) {
+    const std::string firstFileName = "first_settings.txt";
+    const std::string secondFileName = "second_settings.txt";
+    
+    std::ofstream firstFile(firstFileName);
+    firstFile << "Existing content";
+    firstFile.close();
+
+    auto firstInstance = Settings::getInstance(firstFileName);
+
+    std::ofstream secondFile(secondFileName);
+    secondFile << "Updated content";
+    secondFile.close();
+
+    auto secondInstance = Settings::getInstance(secondFileName);
+
+    EXPECT_EQ(firstInstance, secondInstance);
+
+    std::remove(firstFileName.c_str());
+    std::remove(secondFileName.c_str());
+}
+
+TEST(SettingsTest, LoadSettingsTest) {
+    // Путь к файлу настроек
+    std::string settingsPath = "settings123.dat";
+
+    // Убедимся, что файл настроек отсутствует
+    if (std::filesystem::exists(settingsPath)) {
+        std::filesystem::remove(settingsPath);
+    }
+
+    // Получаем экземпляр Settings
+    auto settings = Settings::getInstance("");
+
+    // Проверяем, что используются значения по умолчанию
+    EXPECT_EQ(settings->getNumThread(), 4); // Допустим, 4 — это значение по умолчанию
+
+    // Проверяем другие значения по умолчанию
+    EXPECT_EQ(settings->getPathNadezhda(), "./Generator/source/data/Nadezhda");
+
+    // После теста удаляем созданный файл настроек, если он появился
+    std::filesystem::remove(settingsPath);
+}
+
+TEST(TestSettings, TestDefaultLoadSettings) {
   std::shared_ptr<Settings> t =
       Settings::getInstance("test_default_load_settings");
 
@@ -111,7 +155,7 @@ TEST(test_settings, test_default_load_settings)
 <<<<<<< HEAD
 TEST(
     SettingsTest,
-    defaultInitializationWithLoadSettingsWriteCorrectLogicOperation
+    DefaultInitializationWithLoadSettingsWriteCorrectLogicOperation
 ) {
   if (!std::filesystem::exists(fileName)) {
     std::shared_ptr<Settings> SetPtr =
@@ -139,7 +183,7 @@ TEST(
 
 TEST(
     SettingsTest,
-    defaultInitializationWithLoadSettingsWriteCorrectOperationsToHierarchy
+    DefaultInitializationWithLoadSettingsWriteCorrectOperationsToHierarchy
 ) {
   if (!std::filesystem::exists(fileName)) {
     std::shared_ptr<Settings> SetPtr =
@@ -170,7 +214,7 @@ TEST(
 
 TEST(
     SettingsTest,
-    defaultInitializationWithLoadSettingsWriteCorrectOperationsToName
+    DefaultInitializationWithLoadSettingsWriteCorrectOperationsToName
 ) {
   if (!std::filesystem::exists(fileName)) {
     std::shared_ptr<Settings> SetPtr =
