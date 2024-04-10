b1c957eccbd13b1596c429801e3d3b5435a5263d
magomed <mrnavruzov@edu.hse.ru>
2024-04-10 16:39:50 +0300

    Add new tests to OrientedGraphTests.cpp

--- test/src/baseStructures/graph/OrientedGraphTests.cpp
+++ test/src/baseStructures/graph/OrientedGraphTests.cpp
@@ -4,6 +4,18 @@
 #include "baseStructures/graph/OrientedGraph.hpp"
 
 #include <gtest/gtest.h>
+/*
+  Need to add toGrapMl, toVerilog
+  Need to realize getVerticesByLevel, needToUpdateLevel, updateLevels,
+  getMaxLevel
+
+  Questions in:
+    TestSetNameAndGetName
+    TestIsEmptyAndIsEmptyFull
+    TestGetEdgesCount
+    TestGetVerticesByName
+    TestCalculateHash
+*/
 
 // Test is on top because it needs to contain the graph_0
 TEST(TestSetNameAndGetName, ReturnCorrectName) {
@@ -12,9 +24,11 @@ TEST(TestSetNameAndGetName, ReturnCorrectName) {
   EXPECT_EQ(example1.getName(), "graph_0");
   EXPECT_EQ(example2.getName(), "graph_1");
 
+  // Should be names of copying graph and new grah the same
   // OrientedGraph example3(example1);
   // EXPECT_EQ(example3.getName(), "graph_2");
 
+  // Should be names of copying graph and new grah the same
   // OrientedGraph example4 = example1;
   // EXPECT_EQ(example4.getName(), "graph_3");
 
@@ -69,6 +83,7 @@ TEST(TestBaseSizeAndFullSizeAndSumFullSize, ReturnCorrectSize) {
   subGraph1->addConst('x', "Anything");
   EXPECT_EQ(example.baseSize(), 18);
   EXPECT_EQ(example.fullSize(), 19);
+  // Does sumFullSize() return sum from subGraphs too?
   // EXPECT_EQ(example.sumFullSize(), 28);
 
   auto subGraph2 = example.addSubGraph("SubGraph2");
@@ -78,6 +93,7 @@ TEST(TestBaseSizeAndFullSizeAndSumFullSize, ReturnCorrectSize) {
   subGraph2->addConst('x', "Anything");
   EXPECT_EQ(example.baseSize(), 18);
   EXPECT_EQ(example.fullSize(), 20);
+  // Does sumFullSize() return sum from subGraphs too?
   // EXPECT_EQ(example.sumFullSize(), 32);
 }
 
@@ -116,6 +132,7 @@ TEST(TestIsEmptyAndIsEmptyFull, ReturnCorrectSize) {
 
   auto subGraph = example5.addSubGraph("SubGraph");
   EXPECT_EQ(example5.isEmpty(), true);
+  // Does graph with empty subGraph isEmptyFull?
   // EXPECT_EQ(example5.isEmptyFull(), false);
   subGraph->addInput("Anything");
   EXPECT_EQ(example5.isEmpty(), true);
@@ -197,15 +214,11 @@ TEST(TestGetEdgesCount, ReturnCorrectCount) {
 
 TEST(TestGetSubGraphs, ReturnCorrectSubGraphs) {
   OrientedGraph example;
+  auto          subVector1 = example.getSubGraphs();
+  EXPECT_EQ(subVector1.size(), 0);
 
-  // Is this test case correct?
-  // auto          subVector1 = example.getSubGraphs();
-  // EXPECT_EQ(subVector1.size(), 0);
-  // EXPECT_EQ(subVector1[0], nullptr);
-  // EXPECT_THROW();
-
-  auto          subGraph1  = example.addSubGraph("SubGraph1");
-  auto          subVector2 = example.getSubGraphs();
+  auto subGraph1  = example.addSubGraph("SubGraph1");
+  auto subVector2 = example.getSubGraphs();
   EXPECT_EQ(subVector2[0], subGraph1);
 
   auto subGraph2  = example.addSubGraph("SubGraph2");
@@ -214,6 +227,42 @@ TEST(TestGetSubGraphs, ReturnCorrectSubGraphs) {
   EXPECT_EQ(subVector3.size(), 2);
   EXPECT_EQ(subVector3[0], subGraph1);
   EXPECT_EQ(subVector3[1], subGraph2);
+}
+
+TEST(TestGetBaseVertexes, ReturnCorrectVertexes) {
+  OrientedGraph example;
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::input].size(), 0);
+
+  // without subGraph
+  auto gate1 = example.addGate(Gates::GateAnd, "Anything");
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::gate].size(), 1);
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::gate][0], gate1);
+
+  auto input1 = example.addInput("Anything");
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::input].size(), 1);
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::input][0], input1);
+
+  auto output1 = example.addOutput("Anything");
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::output].size(), 1);
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::output][0], output1);
+
+  auto const1 = example.addConst('x', "Anything");
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::constant].size(), 1);
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::constant][0], const1);
+
+  // with subGraph
+  auto subGraph = example.addSubGraph("Anyhting");
+  subGraph->addGate(Gates::GateAnd, "Anything");
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::gate].size(), 1);
+
+  subGraph->addInput("Anything");
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::input].size(), 1);
+
+  subGraph->addOutput("Anything");
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::output].size(), 1);
+
+  subGraph->addConst('x', "Anything");
+  EXPECT_EQ(example.getBaseVertexes()[VertexTypes::constant].size(), 1);
 }
 
 TEST(TestGetVerticeByIndex, ReturnCorrectType) {
@@ -274,4 +323,165 @@ TEST(TestGetVerticeByIndex, ThrowExceptionWhenWrongIndex) {
   EXPECT_THROW(example.getVerticeByIndex(0), std::invalid_argument);
   example.addGate(Gates::GateAnd, "Anything");
   EXPECT_THROW(example.getVerticeByIndex(1), std::invalid_argument);
+}
+
+TEST(TestGetVerticesByLevel, ReturnCorrectVertices) {
+  // OrientedGraph example;
+  // EXPECT_EQ(example.getVerticesByLevel(0).size(), 0);
+
+  // Do not work
+  // auto          gate = example.addGate(Gates::GateAnd, "Anything");
+  // EXPECT_EQ(gate->getLevel(), 0);
+  // EXPECT_EQ(example.getVerticesByLevel(0)[0]->getType(), VertexTypes::gate);
+}
+
+TEST(TestGetVerticesByName, ReturnCorrectVertices) {
+  OrientedGraph example;
+  // Without subGraph
+  EXPECT_EQ(example.getVerticesByName("Anything").size(), 0);
+
+  example.addGate(Gates::GateAnd, "Anything");
+  EXPECT_EQ(example.getVerticesByName("Anything").size(), 1);
+  EXPECT_EQ(
+      example.getVerticesByName("Anything")[0]->getGate(), Gates::GateAnd
+  );
+
+  example.addInput("Anything");
+  EXPECT_EQ(example.getVerticesByName("Anything").size(), 2);
+  EXPECT_EQ(
+      example.getVerticesByName("Anything")[0]->getType(), VertexTypes::input
+  );
+  EXPECT_EQ(
+      example.getVerticesByName("Anything")[1]->getGate(), Gates::GateAnd
+  );
+
+  example.addOutput("Anything");
+  EXPECT_EQ(example.getVerticesByName("Anything").size(), 3);
+  // Should VertexTypes::output be in the end or be after VertexTypes::input?
+  // EXPECT_EQ(
+  //    example.getVerticesByName("Anything")[2]->getType(), VertexTypes::output
+  //);
+
+  example.addConst('x', "Anything");
+  EXPECT_EQ(example.getVerticesByName("Anything").size(), 4);
+  // What should return at first VertexTypes::constant or VertexTypes::output?
+  // EXPECT_EQ(
+  //    example.getVerticesByName("Anything")[1]->getType(),
+  //    VertexTypes::constant
+  //);
+
+  // With subGraph
+  auto subGraph = example.addSubGraph("Anything");
+  subGraph->addGate(Gates::GateAnd, "Anything");
+  EXPECT_EQ(example.getVerticesByName("Anything").size(), 4);
+  EXPECT_EQ(example.getVerticesByName("Anything", true).size(), 5);
+  EXPECT_EQ(
+      example.getVerticesByName("Anything", true)[4]->getGate(), Gates::GateAnd
+  );
+}
+
+TEST(TestCalculateHash, GraphsWithTheSameStrutureHaveEqualHash) {
+  OrientedGraph graph1;
+  OrientedGraph graph2;
+
+  EXPECT_EQ(graph1.calculateHash(), graph1.calculateHash(true));
+  EXPECT_EQ(graph1.calculateHash(), graph2.calculateHash());
+
+  graph1.addGate(Gates::GateAnd, "Anything");
+  graph2.addGate(Gates::GateAnd, "Anything");
+  EXPECT_EQ(graph1.calculateHash(true), graph2.calculateHash(true));
+
+  graph1.addGate(Gates::GateNand, "Anything");
+  // Should hash be not the same if there are differenece in Gates
+  // EXPECT_NE(graph1.calculateHash(true), graph2.calculateHash(true));
+
+  graph1.addInput("Anything");
+  EXPECT_NE(graph1.calculateHash(true), graph2.calculateHash(true));
+  graph2.addInput("Anything");
+  EXPECT_EQ(graph1.calculateHash(true), graph2.calculateHash(true));
+
+  graph1.addOutput("Anything");
+  // Should hash be not the same if there are differenece in Outputs
+  // EXPECT_NE(graph1.calculateHash(true), graph2.calculateHash(true));
+  graph2.addOutput("Anything");
+  EXPECT_EQ(graph1.calculateHash(true), graph2.calculateHash(true));
+}
+
+TEST(TestGetGatesCount, ReturnCorrectGates) {
+  OrientedGraph example;
+  EXPECT_EQ(example.getGatesCount().size(), 8);
+
+  example.addGate(Gates::GateAnd, "Anything");
+  example.addGate(Gates::GateNand, "Anything");
+  example.addGate(Gates::GateOr, "Anything");
+  example.addGate(Gates::GateNor, "Anything");
+  example.addGate(Gates::GateXor, "Anything");
+  example.addGate(Gates::GateXnor, "Anything");
+  example.addGate(Gates::GateNot, "Anything");
+  example.addGate(Gates::GateBuf, "Anything");
+  example.addGate(Gates::GateDefault, "Anything");
+  example.addInput("Anything");
+  example.addOutput("Anything");
+  example.addConst('x', "Anything");
+  EXPECT_EQ(example.getGatesCount().size(), 9);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateAnd], 1);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateNand], 1);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateOr], 1);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateNor], 1);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateXor], 1);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateXnor], 1);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateNot], 1);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateBuf], 1);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateDefault], 1);
+
+  example.addGate(Gates::GateAnd, "Anything");
+  example.addGate(Gates::GateNand, "Anything");
+  example.addGate(Gates::GateOr, "Anything");
+  example.addGate(Gates::GateNor, "Anything");
+  example.addGate(Gates::GateXor, "Anything");
+  example.addInput("Anything");
+  example.addOutput("Anything");
+  example.addConst('x', "Anything");
+  EXPECT_EQ(example.getGatesCount().size(), 9);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateAnd], 2);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateNand], 2);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateOr], 2);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateNor], 2);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateXor], 2);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateXnor], 1);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateNot], 1);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateBuf], 1);
+  EXPECT_EQ(example.getGatesCount()[Gates::GateDefault], 1);
+}
+
+TEST(TestGetEdgesGatesCount, ReturnCorrectGates) {
+  OrientedGraph example;
+  EXPECT_EQ(example.getEdgesGatesCount().size(), 8);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateAnd].size(), 8);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateNand].size(), 8);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateOr].size(), 8);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateNor].size(), 8);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateXor].size(), 8);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateXnor].size(), 8);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateNot].size(), 8);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateBuf].size(), 8);
+
+  auto gate1 = example.addGate(Gates::GateAnd, "Anything");
+  auto gate2 = example.addGate(Gates::GateNand, "Anything");
+  example.addEdge(gate1, gate2);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateAnd][Gates::GateNand], 1);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateAnd][Gates::GateOr], 0);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateAnd][Gates::GateAnd], 0);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateAnd][Gates::GateNor], 0);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateAnd][Gates::GateXor], 0);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateAnd][Gates::GateXnor], 0);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateAnd][Gates::GateNot], 0);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateAnd][Gates::GateBuf], 0);
+
+  auto gate3 = example.addGate(Gates::GateNand, "Anything");
+  auto gate4 = example.addGate(Gates::GateOr, "Anything");
+  example.addEdges({gate3, gate4}, gate1);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateNand][Gates::GateAnd], 1);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateOr][Gates::GateAnd], 1);
+  EXPECT_EQ(example.getEdgesGatesCount()[Gates::GateAnd][Gates::GateNand], 1);
 }
