e9efc6bd8bc297f8dbf31d94a25cb19ef611a44a
magomed <mrnavruzov@edu.hse.ru>
2024-04-14 19:59:42 +0300

    Fix DataBaseGeneratorTests.cpp

--- test/src/database/DataBaseGeneratorTests.cpp
+++ test/src/database/DataBaseGeneratorTests.cpp
@@ -1,4 +1,6 @@
 #include <filesystem>
+#include <regex>
+#include <string>
 
 #include "database/DataBaseGenerator.hpp"
 
@@ -7,46 +9,187 @@
 #include "additional/filesTools/FilesTools.hpp"
 #include "circuit/Circuit.hpp"
 
-/*TEST(GenerateDataBaseFromRandomTruthTable, EqualWithTheSameSeeds) {
-  std::filesystem::remove_all("dataset/FromRandomTruthTable");
-  GenerationParameters gParams1("FromRandomTruthTable", "1", 5, 5, 1);
-  gParams1.setCNFT(true);
-  DataBaseGeneratorParameters dbParams1(
-      1, 5, 1, 5, 1, FromRandomTruthTable, gParams1
-  );
-  DataBaseGenerator generator1(dbParams1);
-  generator1.generateType(dbParams1);
+std::string unifySchemas(const std::string& schema) {
+  // Регулярное выражение для поиска индексов после символа '_'
+  std::regex  indexRegex("_\\d+");
 
-  GenerationParameters gParams2("FromRandomTruthTable", "2", 5, 5, 1);
-  gParams2.setCNFT(true);
-  DataBaseGeneratorParameters dbParams2(
-      1, 5, 1, 5, 1, FromRandomTruthTable, gParams2
-  );
-  DataBaseGenerator generator2(dbParams2);
-  generator2.generateType(dbParams2);
-}*/
+  // Формат для замены индексов
+  std::string replacement = "_INDEX";
 
-/*TEST(GenerateDataBaseRandLevel, EqualWithTheSameSeeds) {
+  // Замена всех индексов на общий формат
+  std::string unifiedSchema =
+      std::regex_replace(schema, indexRegex, replacement);
+
+  return unifiedSchema;
+}
+
+// TEST(GenerateDataBaseFromRandomTruthTable, EqualWithTheSameSeeds) {
+//   std::filesystem::remove_all("dataset/FromRandomTruthTable");
+//   GenerationParameters gParams1("FromRandomTruthTable", "1", 5, 5, 1);
+//   gParams1.setCNFT(true);
+//   DataBaseGeneratorParameters dbParams1(
+//       1, 5, 1, 5, 1, FromRandomTruthTable, gParams1
+//   );
+//   DataBaseGenerator generator1(dbParams1);
+//   generator1.generateType(dbParams1);
+
+//   GenerationParameters gParams2("FromRandomTruthTable", "2", 5, 5, 1);
+//   gParams2.setCNFT(true);
+//   DataBaseGeneratorParameters dbParams2(
+//       1, 5, 1, 5, 1, FromRandomTruthTable, gParams2
+//   );
+//   DataBaseGenerator generator2(dbParams2);
+//   generator2.generateType(dbParams2);
+// }
+
+TEST(GenerateDataBaseRandLevel, EqualWithTheSameSeeds) {
   std::filesystem::remove_all("dataset/RandLevel");
   GenerationParameters gParams1("RandLevel", "1", 5, 5, 1);
   gParams1.setRandLevelParameters(1, 5, 1, 5);
   DataBaseGeneratorParameters dbParams1(1, 5, 1, 5, 1, RandLevel, gParams1);
   DataBaseGenerator           generator1(dbParams1);
   generator1.generateType(dbParams1);
-  std::string str1 =
-      FilesTools::loadStringFile("dataset/RandLevel/1/CCGRCG0/CCGRCG0.v");
 
   GenerationParameters gParams2("RandLevel", "2", 5, 5, 1);
   gParams2.setRandLevelParameters(1, 5, 1, 5);
   DataBaseGeneratorParameters dbParams2(1, 5, 1, 5, 1, RandLevel, gParams2);
   DataBaseGenerator           generator2(dbParams2);
   generator2.generateType(dbParams2);
-  std::string str2 =
-      FilesTools::loadStringFile("dataset/RandLevel/2/CCGRCG0/CCGRCG0.v");
-  EXPECT_EQ(str1, str2);
-}*/
 
-/*TEST(generateDataBaseNumOperations, EqualWithTheSameSeeds) {
+  // 0
+  int         count = 0;
+  std::string str1  = FilesTools::loadStringFile(
+      "dataset/RandLevel/1/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  std::string str2 = FilesTools::loadStringFile(
+      "dataset/RandLevel/2/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 1
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevel/1/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevel/2/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 2
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevel/1/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevel/2/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 3
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevel/1/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevel/2/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 4
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevel/1/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevel/2/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 5
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevel/1/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevel/2/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 6
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevel/1/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevel/2/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 7
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevel/1/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevel/2/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 8
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevel/1/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevel/2/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 9
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevel/1/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevel/2/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 10
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevel/1/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevel/2/CCGRCG" + std::to_string(count) + "/CCGRCG"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+}
+
+TEST(generateDataBaseNumOperations, EqualWithTheSameSeeds) {
   std::filesystem::remove_all("data/NumOperations");
   GenerationParameters gParams1("NumOperations", "1", 5, 5, 1);
   std::map<Gates, int> logicOper = {
@@ -67,9 +210,45 @@
   DataBaseGeneratorParameters dbParams2(1, 5, 1, 5, 1, NumOperation, gParams2);
   DataBaseGenerator           generator2(dbParams2);
   generator2.generateType(dbParams2);
-}*/
 
-/*TEST(GenerateDataBaseRandLevelExperimental, EqualWithTheSameSeeds) {
+  // 0
+  int         count = 0;
+  std::string str1  = FilesTools::loadStringFile(
+      "dataset/NumOperations/1/CCGRVC" + std::to_string(count) + "/CCGRVC"
+      + std::to_string(count) + ".v"
+  );
+  std::string str2 = FilesTools::loadStringFile(
+      "dataset/NumOperations/2/CCGRVC" + std::to_string(count) + "/CCGRVC"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 1
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/NumOperations/1/CCGRVC" + std::to_string(count) + "/CCGRVC"
+      + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/NumOperations/2/CCGRVC" + std::to_string(count) + "/CCGRVC"
+      + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  //   // 2
+  //   count++;
+  //   str1 = FilesTools::loadStringFile(
+  //       "dataset/NumOperations/1/CCGRVC" + std::to_string(count) + "/CCGRVC"
+  //       + std::to_string(count) + ".v"
+  //   );
+  //   str2 = FilesTools::loadStringFile(
+  //       "dataset/NumOperations/2/CCGRVC" + std::to_string(count) + "/CCGRVC"
+  //       + std::to_string(count) + ".v"
+  //   );
+  //   EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+}
+
+TEST(GenerateDataBaseRandLevelExperimental, EqualWithTheSameSeeds) {
   std::filesystem::remove_all("dataset/RandLevelExperimental");
   GenerationParameters gParams1("RandLevelExperimental", "1", 5, 5, 1);
   gParams1.setRandLevelParameters(1, 5, 1, 5);
@@ -78,9 +257,6 @@
   );
   DataBaseGenerator generator1(dbParams1);
   generator1.generateType(dbParams1);
-  std::string str1 = FilesTools::loadStringFile(
-      "dataset/RandLevelExperimental/1/CCGRCG0/CCGRCG0.v"
-  );
 
   GenerationParameters gParams2("RandLevelExperimental", "2", 5, 5, 1);
   gParams2.setRandLevelParameters(1, 5, 1, 5);
@@ -89,8 +265,136 @@
   );
   DataBaseGenerator generator2(dbParams2);
   generator2.generateType(dbParams2);
-  std::string str2 = FilesTools::loadStringFile(
-      "dataset/RandLevelExperimental/2/CCGRCG0/CCGRCG0.v"
+
+  // 0
+  int         count = 0;
+  std::string str1  = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/1/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
   );
-  EXPECT_EQ(str1, str2);
-}*/
+  std::string str2 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/2/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 1
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/1/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/2/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 2
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/1/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/2/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 3
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/1/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/2/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 4
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/1/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/2/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 5
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/1/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/2/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 6
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/1/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/2/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 7
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/1/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/2/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 8
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/1/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/2/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 9
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/1/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/2/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+
+  // 10
+  count++;
+  str1 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/1/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  str2 = FilesTools::loadStringFile(
+      "dataset/RandLevelExperimental/2/CCGRCGE" + std::to_string(count)
+      + "/CCGRCGE" + std::to_string(count) + ".v"
+  );
+  EXPECT_EQ(unifySchemas(str1), unifySchemas(str2));
+}
