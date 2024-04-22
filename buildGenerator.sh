f5f0e6eef463800d2b0243c303f925ada7513158
Fuuulkrum7 <ilka747428@gmail.com>
2024-04-22 19:38:49 +0300

    Add threads for cmake

--- buildGenerator.sh
+++ buildGenerator.sh
@@ -2,5 +2,5 @@
 if [ ! -d "build" ]; then
   mkdir build
 fi
-cmake -S . -B build -D CMAKE_BUILD_TYPE=Release
-cmake --build build
+cmake -S . -B build -D CMAKE_BUILD_TYPE=Release -j$(nproc)
+cmake --build build -j$(nproc)
