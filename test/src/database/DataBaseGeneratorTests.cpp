bf61c580fd0bf9dd24616bf0b2672d2b28fa1eda
magomed <mrnavruzov@edu.hse.ru>
2024-04-22 13:14:30 +0300

    Update DataBaseGeneratorTests.cpp, change way of getting json files

--- test/src/database/DataBaseGeneratorTests.cpp
+++ test/src/database/DataBaseGeneratorTests.cpp
@@ -1,5 +1,4 @@
-#include <filesystem>
-#include <regex>
+#include <filesystem>|
 #include <string>
 
 #include "database/DataBaseGenerator.hpp"
@@ -9,18 +8,25 @@
 #include "additional/filesTools/FilesTools.hpp"
 #include "circuit/Circuit.hpp"
 
-int countFilesInDirectory(const std::string& directoryPath) {
-  int fileCount = 0;
-  for (const auto& entry : std::filesystem::directory_iterator(directoryPath)) {
-    if (std::filesystem::is_directory(entry)) {
-      ++fileCount;
+std::vector<std::string> getJsons(std::string& path) {
+  std::vector<std::string> names = FilesTools::getDirectories(path);
+  std::sort(names.begin(), names.end());
+  std::vector<std::string> jsons;
+  for (const auto& name : names) {
+    if (!std::filesystem::is_empty(path + "/" + name)) {
+      std::string json = path + "/" + name + "/" + name + ".json";
+      if (std::filesystem::exists(json))
+        jsons.push_back(json);
     }
   }
-  return fileCount;
+  return jsons;
 }
 
 TEST(GenerateDataBaseFromRandomTruthTable, EqualWithTheSameSeeds) {
-  GenerationParameters gParams1("FromRandomTruthTable", "1", 1, 1, 1);
+  std::string          mainDir = "dataset";
+  std::string          dir     = "FromRandomTruthTable";
+
+  GenerationParameters gParams1(dir, "1", 1, 1, 1);
   gParams1.setCNFT(true);
   DataBaseGeneratorParameters dbParams1(
       1, 4, 1, 5, 1, FromRandomTruthTable, gParams1
@@ -28,7 +34,7 @@ TEST(GenerateDataBaseFromRandomTruthTable, EqualWithTheSameSeeds) {
   DataBaseGenerator generator1(dbParams1);
   generator1.generateType(dbParams1);
 
-  GenerationParameters gParams2("FromRandomTruthTable", "2", 1, 1, 1);
+  GenerationParameters gParams2(dir, "2", 1, 1, 1);
   gParams2.setCNFT(true);
   DataBaseGeneratorParameters dbParams2(
       1, 4, 1, 5, 1, FromRandomTruthTable, gParams2
@@ -36,19 +42,19 @@ TEST(GenerateDataBaseFromRandomTruthTable, EqualWithTheSameSeeds) {
   DataBaseGenerator generator2(dbParams2);
   generator2.generateType(dbParams2);
 
-  std::string path1     = "dataset/FromRandomTruthTable/1/";
-  std::string path2     = "dataset/FromRandomTruthTable/2/";
-  int         fileCount = countFilesInDirectory(path1);
-  for (int i = 2; i < fileCount; ++i) {
-    std::string str1 = FilesTools::loadStringFile(
-        path1 + "CCGRTT" + std::to_string(i) + "_CNFT" + "/CCGRTT"
-        + std::to_string(i) + "_CNFT" + ".json"
+  // receive all json files
+  std::string              dir1   = mainDir + "/" + dir + "/" + "1";
+  std::string              dir2   = mainDir + "/" + dir + "/" + "2";
+  std::vector<std::string> jsons1 = getJsons(dir1);
+  std::vector<std::string> jsons2 = getJsons(dir2);
+  ASSERT_EQ(jsons1.empty(), false);
+  ASSERT_EQ(jsons1.size(), jsons2.size());
+
+  for (int i = 0; i < jsons1.size(); ++i) {
+    EXPECT_EQ(
+        FilesTools::loadStringFile(jsons1[i]),
+        FilesTools::loadStringFile(jsons2[i])
     );
-    std::string str2 = FilesTools::loadStringFile(
-        path2 + "CCGRTT" + std::to_string(i) + "_CNFT" + "/CCGRTT"
-        + std::to_string(i) + "_CNFT" + ".json"
-    );
-    EXPECT_EQ(str1, str2);
   }
 
   GenerationParameters gParams3("FromRandomTruthTable", "3", 1, 1, 1);
@@ -67,107 +73,128 @@ TEST(GenerateDataBaseFromRandomTruthTable, EqualWithTheSameSeeds) {
   DataBaseGenerator generator4(dbParams4);
   generator4.generateType(dbParams4);
 
-  path1     = "dataset/FromRandomTruthTable/3/";
-  path2     = "dataset/FromRandomTruthTable/4/";
-  fileCount = countFilesInDirectory(path1);
-  for (int i = 2; i < fileCount; ++i) {
-    std::string str1 = FilesTools::loadStringFile(
-        path1 + "CCGRTT" + std::to_string(i) + "_CNFT" + "/CCGRTT"
-        + std::to_string(i) + "_CNFT" + ".json"
+  dir1   = mainDir + "/" + dir + "/" + "3";
+  dir2   = mainDir + "/" + dir + "/" + "4";
+  jsons1 = getJsons(dir1);
+  jsons2 = getJsons(dir2);
+  ASSERT_EQ(jsons1.empty(), false);
+  ASSERT_EQ(jsons1.size(), jsons2.size());
+
+  for (int i = 0; i < jsons1.size(); ++i) {
+    EXPECT_EQ(
+        FilesTools::loadStringFile(jsons1[i]),
+        FilesTools::loadStringFile(jsons2[i])
     );
-    std::string str2 = FilesTools::loadStringFile(
-        path2 + "CCGRTT" + std::to_string(i) + "_CNFT" + "/CCGRTT"
-        + std::to_string(i) + "_CNFT" + ".json"
-    );
-    EXPECT_EQ(str1, str2);
   }
-  std::filesystem::remove_all("dataset/FromRandomTruthTable");
+  std::filesystem::remove_all(mainDir + "/" + dir);
 }
 
 TEST(GenerateDataBaseRandLevel, EqualWithTheSameSeeds) {
-  GenerationParameters gParams1("RandLevel", "1", 5, 5, 1);
+  std::string          mainDir = "dataset";
+  std::string          dir     = "RandLevel";
+
+  GenerationParameters gParams1(dir, "1", 1, 1, 1);
   gParams1.setRandLevelParameters(1, 5, 1, 5);
   DataBaseGeneratorParameters dbParams1(1, 5, 1, 5, 1, RandLevel, gParams1);
   DataBaseGenerator           generator1(dbParams1);
   generator1.generateType(dbParams1);
 
-  GenerationParameters gParams2("RandLevel", "2", 5, 5, 1);
+  GenerationParameters gParams2(dir, "2", 1, 1, 1);
   gParams2.setRandLevelParameters(1, 5, 1, 5);
   DataBaseGeneratorParameters dbParams2(1, 5, 1, 5, 1, RandLevel, gParams2);
   DataBaseGenerator           generator2(dbParams2);
   generator2.generateType(dbParams2);
 
-  std::string path1     = "dataset/RandLevel/1/";
-  std::string path2     = "dataset/RandLevel/2/";
-  int         fileCount = countFilesInDirectory(path1);
-  for (int i = 0; i < fileCount; ++i) {
-    std::string str1 = FilesTools::loadStringFile(
-        path1 + "CCGRCG" + std::to_string(i) + "/CCGRCG" + std::to_string(i)
-        + ".json"
+  std::string              dir1   = mainDir + "/" + dir + "/" + "1";
+  std::string              dir2   = mainDir + "/" + dir + "/" + "2";
+  std::vector<std::string> jsons1 = getJsons(dir1);
+  std::vector<std::string> jsons2 = getJsons(dir2);
+  ASSERT_EQ(jsons1.empty(), false);
+  ASSERT_EQ(jsons1.size(), jsons2.size());
+
+  for (int i = 0; i < jsons1.size(); ++i) {
+    EXPECT_EQ(
+        FilesTools::loadStringFile(jsons1[i]),
+        FilesTools::loadStringFile(jsons2[i])
     );
-    std::string str2 = FilesTools::loadStringFile(
-        path2 + "CCGRCG" + std::to_string(i) + "/CCGRCG" + std::to_string(i)
-        + ".json"
-    );
-    EXPECT_EQ(str1, str2);
   }
 
-  GenerationParameters gParams3("RandLevel", "3", 1, 1, 1);
+  GenerationParameters gParams3(dir, "3", 1, 1, 1);
   gParams3.setRandLevelParameters(1, 10, 1, 10);
   DataBaseGeneratorParameters dbParams3(2, 6, 2, 6, 4, RandLevel, gParams3);
   DataBaseGenerator           generator3(dbParams3);
   generator1.generateType(dbParams3);
 
-  GenerationParameters gParams4("RandLevel", "4", 1, 1, 1);
+  GenerationParameters gParams4(dir, "4", 1, 1, 1);
   gParams4.setRandLevelParameters(1, 10, 1, 10);
   DataBaseGeneratorParameters dbParams4(2, 6, 2, 6, 4, RandLevel, gParams4);
   DataBaseGenerator           generator4(dbParams4);
   generator2.generateType(dbParams4);
 
-  path1     = "dataset/RandLevel/3/";
-  path2     = "dataset/RandLevel/4/";
-  fileCount = countFilesInDirectory(path1);
-  for (int i = 0; i < fileCount; ++i) {
-    std::string str1 = FilesTools::loadStringFile(
-        path1 + "CCGRCG" + std::to_string(i) + "/CCGRCG" + std::to_string(i)
-        + ".json"
+  dir1   = mainDir + "/" + dir + "/" + "3";
+  dir2   = mainDir + "/" + dir + "/" + "4";
+  jsons1 = getJsons(dir1);
+  jsons2 = getJsons(dir2);
+  ASSERT_EQ(jsons1.empty(), false);
+  ASSERT_EQ(jsons1.size(), jsons2.size());
+
+  for (int i = 0; i < jsons1.size(); ++i) {
+    EXPECT_EQ(
+        FilesTools::loadStringFile(jsons1[i]),
+        FilesTools::loadStringFile(jsons2[i])
     );
-    std::string str2 = FilesTools::loadStringFile(
-        path2 + "CCGRCG" + std::to_string(i) + "/CCGRCG" + std::to_string(i)
-        + ".json"
-    );
-    EXPECT_EQ(str1, str2);
   }
-  std::filesystem::remove_all("dataset/RandLevel");
+  std::filesystem::remove_all(mainDir + "/" + dir);
 }
 
 // Circuits have defferent hashes
-// TEST(generateDataBaseNumOperations, EqualWithTheSameSeeds) {
-//   GenerationParameters gParams1("NumOperations", "1", 5, 5, 1);
-//   std::map<Gates, int> logicOper = {
-//       {Gates::GateAnd, 5},
-//       {Gates::GateOr, 5},
-//       {Gates::GateNot, 6},
-//       {Gates::GateNor, 6},
-//       {Gates::GateNand, 1},
-//       {Gates::GateXnor, 9},
-//       {Gates::GateXor, 12}};
-//   gParams1.setNumOperationParameters(logicOper, false);
-//   DataBaseGeneratorParameters dbParams1(1, 5, 1, 5, 1, NumOperation,
-//   gParams1); DataBaseGenerator           generator1(dbParams1);
-//   generator1.generateType(dbParams1);
+TEST(generateDataBaseNumOperations, EqualWithTheSameSeeds) {
+  std::string          mainDir   = "dataset";
+  std::string          dir       = "NumOperations";
 
-//   GenerationParameters gParams2("NumOperations", "2", 5, 5, 1);
-//   gParams2.setNumOperationParameters(logicOper, false);
-//   DataBaseGeneratorParameters dbParams2(1, 5, 1, 5, 1, NumOperation,
-//   gParams2); DataBaseGenerator           generator2(dbParams2);
-//   generator2.generateType(dbParams2);
+  std::map<Gates, int> logicOper = {
+      {Gates::GateAnd, 5},
+      {Gates::GateOr, 5},
+      {Gates::GateNot, 6},
+      {Gates::GateNor, 6},
+      {Gates::GateNand, 1},
+      {Gates::GateXnor, 9},
+      {Gates::GateXor, 12}};
+  GenerationParameters gParams1(dir, "1", 5, 5, 1);
+  gParams1.setNumOperationParameters(logicOper, false);
+  DataBaseGeneratorParameters dbParams1(1, 5, 1, 5, 1, NumOperation, gParams1);
+  DataBaseGenerator           generator1(dbParams1);
+  generator1.generateType(dbParams1);
 
-//   std::filesystem::remove_all("dataset/NumOperations");
-// }
+  GenerationParameters gParams2(dir, "2", 5, 5, 1);
+  gParams2.setNumOperationParameters(logicOper, false);
+  DataBaseGeneratorParameters dbParams2(1, 5, 1, 5, 1, NumOperation, gParams2);
+  DataBaseGenerator           generator2(dbParams2);
+  generator2.generateType(dbParams2);
+
+  // receive all json files
+  std::string              dir1   = mainDir + "/" + dir + "/" + "1";
+  std::string              dir2   = mainDir + "/" + dir + "/" + "2";
+  std::vector<std::string> jsons1 = getJsons(dir1);
+  std::vector<std::string> jsons2 = getJsons(dir2);
+  ASSERT_EQ(jsons1.empty(), false);
+  ASSERT_EQ(jsons1.size(), jsons2.size());
+
+  for (int i = 0; i < jsons1.size(); ++i) {
+    EXPECT_EQ(
+        FilesTools::loadStringFile(jsons1[i]),
+        FilesTools::loadStringFile(jsons2[i])
+    );
+  }
+
+  std::filesystem::remove_all(mainDir + "/" + dir);
+}
 
 TEST(GenerateDataBaseRandLevelExperimental, EqualWithTheSameSeeds) {
-  GenerationParameters gParams1("RandLevelExperimental", "1", 5, 5, 1);
+  std::string          mainDir = "dataset";
+  std::string          dir     = "RandLevelExperimental";
+
+  GenerationParameters gParams1(dir, "1", 5, 5, 1);
   gParams1.setRandLevelParameters(1, 5, 1, 5);
   DataBaseGeneratorParameters dbParams1(
       1, 5, 1, 5, 1, RandLevelExperimental, gParams1
@@ -175,7 +202,7 @@ TEST(GenerateDataBaseRandLevelExperimental, EqualWithTheSameSeeds) {
   DataBaseGenerator generator1(dbParams1);
   generator1.generateType(dbParams1);
 
-  GenerationParameters gParams2("RandLevelExperimental", "2", 5, 5, 1);
+  GenerationParameters gParams2(dir, "2", 5, 5, 1);
   gParams2.setRandLevelParameters(1, 5, 1, 5);
   DataBaseGeneratorParameters dbParams2(
       1, 5, 1, 5, 1, RandLevelExperimental, gParams2
@@ -183,22 +210,22 @@ TEST(GenerateDataBaseRandLevelExperimental, EqualWithTheSameSeeds) {
   DataBaseGenerator generator2(dbParams2);
   generator2.generateType(dbParams2);
 
-  std::string path1     = "dataset/RandLevelExperimental/1/";
-  std::string path2     = "dataset/RandLevelExperimental/2/";
-  int         fileCount = countFilesInDirectory(path1);
-  for (int i = 0; i < fileCount; ++i) {
-    std::string str1 = FilesTools::loadStringFile(
-        path1 + "CCGRCGE" + std::to_string(i) + "/CCGRCGE" + std::to_string(i)
-        + ".json"
+  // receive all json files
+  std::string              dir1   = mainDir + "/" + dir + "/" + "1";
+  std::string              dir2   = mainDir + "/" + dir + "/" + "2";
+  std::vector<std::string> jsons1 = getJsons(dir1);
+  std::vector<std::string> jsons2 = getJsons(dir2);
+  ASSERT_EQ(jsons1.empty(), false);
+  ASSERT_EQ(jsons1.size(), jsons2.size());
+
+  for (int i = 0; i < jsons1.size(); ++i) {
+    EXPECT_EQ(
+        FilesTools::loadStringFile(jsons1[i]),
+        FilesTools::loadStringFile(jsons2[i])
     );
-    std::string str2 = FilesTools::loadStringFile(
-        path2 + "CCGRCGE" + std::to_string(i) + "/CCGRCGE" + std::to_string(i)
-        + ".json"
-    );
-    EXPECT_EQ(str1, str2);
   }
 
-  GenerationParameters gParams3("RandLevelExperimental", "3", 1, 1, 1);
+  GenerationParameters gParams3(dir, "3", 1, 1, 1);
   gParams3.setRandLevelParameters(2, 10, 2, 10);
   DataBaseGeneratorParameters dbParams3(
       2, 7, 2, 7, 3, RandLevelExperimental, gParams3
@@ -206,7 +233,7 @@ TEST(GenerateDataBaseRandLevelExperimental, EqualWithTheSameSeeds) {
   DataBaseGenerator generator3(dbParams3);
   generator3.generateType(dbParams3);
 
-  GenerationParameters gParams4("RandLevelExperimental", "4", 1, 1, 1);
+  GenerationParameters gParams4(dir, "4", 1, 1, 1);
   gParams4.setRandLevelParameters(2, 10, 2, 10);
   DataBaseGeneratorParameters dbParams4(
       2, 7, 2, 7, 3, RandLevelExperimental, gParams4
@@ -214,19 +241,18 @@ TEST(GenerateDataBaseRandLevelExperimental, EqualWithTheSameSeeds) {
   DataBaseGenerator generator4(dbParams4);
   generator4.generateType(dbParams4);
 
-  path1     = "dataset/RandLevelExperimental/3/";
-  path2     = "dataset/RandLevelExperimental/4/";
-  fileCount = countFilesInDirectory(path1);
-  for (int i = 0; i < fileCount; ++i) {
-    std::string str1 = FilesTools::loadStringFile(
-        path1 + "CCGRCGE" + std::to_string(i) + "/CCGRCGE" + std::to_string(i)
-        + ".json"
+  dir1   = mainDir + "/" + dir + "/" + "3";
+  dir2   = mainDir + "/" + dir + "/" + "4";
+  jsons1 = getJsons(dir1);
+  jsons2 = getJsons(dir2);
+  ASSERT_EQ(jsons1.empty(), false);
+  ASSERT_EQ(jsons1.size(), jsons2.size());
+
+  for (int i = 0; i < jsons1.size(); ++i) {
+    EXPECT_EQ(
+        FilesTools::loadStringFile(jsons1[i]),
+        FilesTools::loadStringFile(jsons2[i])
     );
-    std::string str2 = FilesTools::loadStringFile(
-        path2 + "CCGRCGE" + std::to_string(i) + "/CCGRCGE" + std::to_string(i)
-        + ".json"
-    );
-    EXPECT_EQ(str1, str2);
   }
-  std::filesystem::remove_all("dataset/RandLevelExperimental");
+  std::filesystem::remove_all(mainDir + "/" + dir);
 }
