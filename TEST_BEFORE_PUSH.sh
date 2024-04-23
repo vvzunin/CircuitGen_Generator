6d9c513eed12ad7a8f9e6186480bdbd070082d6c
Fuuulkrum7 <ilka747428@gmail.com>
2024-04-23 12:26:12 +0300

    Speed up build

--- TEST_BEFORE_PUSH.sh
+++ TEST_BEFORE_PUSH.sh
@@ -1,4 +1,4 @@
 cmake --preset=dev
-cmake --build --preset=dev -t format-fix
-cmake --build --preset=dev
+cmake --build --preset=dev -t format-fix  -j $(nproc)
+cmake --build --preset=dev -j $(nproc)
 ctest --preset=dev
