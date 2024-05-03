bba54b540b9adc442c80052d54bf28f8d8a28415
Your Name <your_email@example.com>
2024-05-03 17:29:18 +0300

    Added almost all description of methods for class OrientedGraph with examples

--- src/baseStructures/graph/OrientedGraph.hpp
+++ src/baseStructures/graph/OrientedGraph.hpp
@@ -136,25 +136,237 @@ public:
 
   void                      updateLevels();
 
+  /// @brief getMaxLevel
+  /// Calculates and returns the maximum level of the output vertices in the
+  /// graph
+  /// @return maximum level of the output vertices
+  /// @code
+  ///  OrientedGraph graph("ExampleGraph");
+  /// // Add vertices and edges to the graph (omitted for brevity)
+  /// // Calculate the maximum level of output vertices
+  /// unsigned maxLevel = graph.getMaxLevel();
+  /// // Output the result
+  /// std::cout << "Maximum level of output vertices: " << maxLevel << '\n';
+  /// @endcode
+
   unsigned                  getMaxLevel();
 
+  /// @brief addParentGraph
+  /// Adds a parent graph to the current graph
+  /// @param i_baseGraph A shared pointer to the parent graph to be added
+  /// @code
+  /// // Create the first OrientedGraph instance
+  /// auto graph1 = std::make_shared<OrientedGraph>("Graph1");
+  /// // Create the second OrientedGraph instance
+  /// auto graph2 = std::make_shared<OrientedGraph>("Graph2");
+  /// // Add graph1 as a parent to graph2
+  /// graph2->addParentGraph(graph1);
+  /// // Now graph2 has graph1 as one of its parent graphs
+  /// @endcode
   void                      addParentGraph(GraphPtr i_baseGraph);
+
+
+  /// @brief getParentGraphs
+  /// Retrieves the parent graphs of the current graph
+  /// @return A vector containing weak pointers to the parent graphs
+  /// @code
+  /// // Create an instance of OrientedGraph
+  /// auto graph = std::make_shared<OrientedGraph>("ExampleGraph");
+  /// // Add some parent graphs to the current graph (omitted for brevity)
+  /// // Retrieve the parent graphs of the current graph
+  /// std::vector<std::weak_ptr<OrientedGraph>> parentGraphs = graph->getParentGraphs();
+  /// // Iterate over the parent graphs and perform operations (omitted for brevity)
+  /// @endcode
+
   std::vector<GraphPtrWeak> getParentGraphs() const;
 
+
+  /// @brief setCurrentParent
+  /// Sets the current parent graph of the current graph
+  /// @param parent A shared pointer to the parent graph to be set as the
+  /// current parent
+  /// @code
+  /// // Create an instance of OrientedGraph
+  /// auto graph = std::make_shared<OrientedGraph>("ExampleGraph");
+  /// // Create another instance of OrientedGraph to be set as the current parent
+  /// auto parentGraph = std::make_shared<OrientedGraph>("ParentGraph");
+  /// // Set parentGraph as the current parent of graph
+  /// graph->setCurrentParent(parentGraph);
+  /// // Now parentGraph is set as the current parent of graph
+  /// @endcode
   void                      setCurrentParent(GraphPtr parent);
+
+
+
+  /// @brief resetCounters
+  /// Resets counters associated with a specific graph instance
+  /// @param where A shared pointer to the subgraph whose counters need to be
+  /// reset
   void                      resetCounters(GraphPtr where);
 
   // TODO: Заменить все const на const &
+
+
+  /// @brief addInput
+  /// Adds an input vertex to the current graph
+  /// @param i_name The name of the input vertex to be added
+  /// @return A shared pointer to the newly created input vertex
+  /// @code
+  /// // Create an instance of OrientedGraph
+  /// auto graph = std::make_shared<OrientedGraph>("ExampleGraph");
+  /// // Add an input vertex to the graph
+  /// auto inputVertex = graph->addInput("Input1");
+  /// // Now inputVertex is a shared pointer to the newly added input vertex
+  /// @endcode
+
   VertexPtr                 addInput(const std::string& i_name = "");
+
+
+  /// @brief addOutput
+  /// Adds an output vertex to the current graph
+  /// @param i_name The name of the output vertex to be added
+  /// @return A shared pointer to the newly created output vertex
+  /// @code
+  /// // Create an instance of OrientedGraph
+  /// auto graph = std::make_shared<OrientedGraph>("ExampleGraph");
+  /// // Add an output vertex to the graph
+  /// auto outputVertex = graph->addOutput("Output1");
+  /// // Now outputVertex is a shared pointer to the newly added output vertex
+  /// @endcode
+
   VertexPtr                 addOutput(const std::string& i_name = "");
+
+
+  /// @brief addConst
+  /// Adds a constant vertex to the current graph
+  /// @param i_value The value of the constant vertex to be added
+  /// @param i_name The name of the constant vertex to be added
+  /// @return A shared pointer to the newly created constant vertex
+  /// @code
+  /// // Create an instance of OrientedGraph
+  /// auto graph = std::make_shared<OrientedGraph>("ExampleGraph");
+  /// // Add a constant vertex to the graph with value 'A' and name "Constant1"
+  /// auto constantVertex = graph->addConst('A', "Constant1");
+  /// // Now constantVertex is a shared pointer to the newly added constant vertex
+  /// @endcode
+
   VertexPtr addConst(const char& i_value, const std::string& i_name = "");
+
+
+
+  /// @brief addGate
+  /// Adds a gate vertex to the current graph
+  /// @param i_gate The type of the gate vertex to be added
+  /// @param i_name The name of the gate vertex to be added
+  /// @return A shared pointer to the newly created gate vertex
+  /// @code
+  /// // Create an instance of OrientedGraph
+  /// auto graph = std::make_shared<OrientedGraph>("ExampleGraph");
+  /// // Add a gate vertex to the graph with type GateAnd and name "Gate1"
+  /// auto gateVertex = graph->addGate(Gates::GateAnd, "Gate1");
+  /// // Now gateVertex is a shared pointer to the newly added gate vertex
+  /// @endcode
+
   VertexPtr addGate(const Gates& i_gate, const std::string& i_name = "");
+
+  
+  /// @brief addSubGraph
+  /// Adds a subgraph to the current graph
+  /// @param i_subGraph A shared pointer to the subgraph to be added
+  /// @param i_inputs A vector containing the input vertices to be connected
+  /// to the subgraph
+  /// @return A vector containing the output vertices of the subgraph
+  /// @code
+  /// // Create an instance of OrientedGraph
+  /// auto graph = std::make_shared<OrientedGraph>("ExampleGraph");
+  /// // Create a subgraph
+  /// auto subGraph = std::make_shared<OrientedGraph>("SubGraph");
+  /// // Create input vertices for the subgraph
+  /// std::vector<VertexPtr> subGraphInputs;
+  /// for (int i = 0; i < 3; ++i)
+  /// {
+  ///   auto inputVertex = graph->addInput("SubGraphInput" + std::to_string(i+1));
+  ///   subGraphInputs.push_back(inputVertex);
+  /// }
+  /// // Add the subgraph to the graph with its inputs
+  /// auto subGraphOutputs = graph->addSubGraph(subGraph, subGraphInputs);
+  /// // Now subGraphOutputs contains the output vertices of the subgraph
+  /// @endcode
+  /// @throws std::invalid_argument if the number of inputs does not match
+  /// the number of input vertices in the subgraph
+
   std::vector<VertexPtr>
          addSubGraph(GraphPtr i_subGraph, std::vector<VertexPtr> i_inputs);
 
+
+  /// @brief addEdge
+  /// Adds an edge between two vertices in the current graph
+  /// @param from A shared pointer to the vertex where the edge originates
+  /// @param to A shared pointer to the vertex where the edge terminates
+  /// @return true if the edge was successfully added, false otherwise
+  /// @code
+  /// // Create an instance of OrientedGraph
+  /// auto graph = std::make_shared<OrientedGraph>("ExampleGraph");
+  /// // Create two vertices
+  /// auto vertex1 = graph->addInput("Vertex1");
+  /// auto vertex2 = graph->addOutput("Vertex2");
+  /// // Add an edge from vertex1 to vertex2
+  /// bool edgeAdded = graph->addEdge(vertex1, vertex2);
+  /// if (edgeAdded)
+  /// {
+  ///   std::cout << "Edge successfully added between Vertex1 and Vertex2\n";
+  /// } 
+  /// else
+  /// {
+  ///   std::cerr << "Failed to add edge between Vertex1 and Vertex2\n";
+  /// }
+  /// @endcode
+  /// @throws std::invalid_argument if attempting to add an edge from one
+  ///subgraph to another when the 'from' vertex is not an output, or when
+  /// attempting to add an edge to another subgraph when the 'to' vertex is
+  /// not an input
+
   bool   addEdge(VertexPtr from, VertexPtr to);
+
+
+  /// @brief addEdges
+  /// Adds multiple edges from a vector of source vertices to a target vertex
+  /// @param from1 A vector containing the source vertices from which edges
+  /// will originate
+  /// @param to A shared pointer to the target vertex where the edges will
+  /// terminate
+  /// @return true if all edges were successfully added, false if at least one
+  /// edge failed to be added
+  /// @code
+  /// // Create an instance of OrientedGraph
+  /// auto graph = std::make_shared<OrientedGraph>("ExampleGraph");
+  /// // Create multiple source vertices
+  /// std::vector<VertexPtr> sources;
+  /// for (int i = 0; i < 3; ++i)
+  /// {
+  ///   auto vertex = graph->addInput("Source" + std::to_string(i+1));
+  ///   sources.push_back(vertex);
+  /// }
+  /// // Create a target vertex
+  /// auto target = graph->addOutput("Target");
+  /// // Add edges from all source vertices to the target vertex
+  /// bool edgesAdded = graph->addEdges(sources, target);
+  /// if (edgesAdded)
+  /// {
+  ///   std::cout << "Edges successfully added from all sources to the target\n";
+  /// } 
+  /// else
+  /// {
+  ///   std::cerr << "Failed to add edges from one or more sources to the target\n";
+  /// }
+  /// @endcode
+
   bool   addEdges(std::vector<VertexPtr> from1, VertexPtr to);
 
+
+  /// @brief getEdgesCount
+  /// Retrieves the total number of edges in the graph
+  /// @return The total number of edges in the graph
   size_t getEdgesCount() { return d_edgesCount; }
 
   std::set<GraphPtr>                            getSubGraphs() const;
