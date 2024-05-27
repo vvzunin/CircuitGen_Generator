292e15c8c9677a12677c1ff2ff40523b252ec353
Fuuulkrum7 <ilka747428@gmail.com>
2024-05-27 12:52:24 +0300

    In fix process

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char **argv) {
     }
   }
   
-  // json_path = "../../examples/json/sampleGenetic.json";
+  json_path = "../../examples/json/sampleGenetic.json";
   // std::clog << std::filesystem::current_path() << std::endl;
   CircuitGenGenerator::runGenerationFromJson(json_path);
 
